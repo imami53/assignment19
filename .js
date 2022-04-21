9b5bf3e52810e0599fea03de4e12c9577f7c6c97
imami53 <ogechiuba53@gmail.com>
2022-04-21 14:36:50 +0100

    Assignment19

---
+++ .js
@@ -0,0 +1,15 @@
+
+
+function lifeInWeeks() {
+    var age = lifeInWeeks
+    var life = parseInt(90 - "age");
+    var M = parseInt(12 * "life");
+    var W = parseInt(4 * "M");
+    var D = parseInt(7 * "W");
+    var output = "You have " + D + "Days" + W + "Weeks" + M + "Months"
+    console.log(output);
+    alert(output);
+}
+// document.getElementById("wrap").innerHTML =
+    // alert(output)
+    lifeInWeeks(30)
